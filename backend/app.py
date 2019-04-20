fefb3dc1a3cb042f8c89a5c2561cbc307ca81a99
Alekhya Akella <47712386+AlekhyaAkella@users.noreply.github.com>
2019-04-20 22:42:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+import json 
+
+app=Flask(__name__)
+
+#error handler for wrong URl
+@app.errorhandler(404)
+def page_not_found(e):
+    return "check your url and try again"
+
+#routing for displaying required output
+@app.route('/api/pokemon/')
+def index():
+    poke_names={"pokemon":["bulbasaur","charmander","squirtle"]}
+    return json.dumps(poke_names)
+    
+if __name__ == '__main__':
+    app.run("localhost",port=8006,debug=True)
 
